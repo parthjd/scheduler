98d46778d1a6af85c5fe4c8897f32f298c0034f0
parthjd <parthjd28@gmail.com>
2020-02-26 18:49:31 -0500

    updated index.js

--- src/index.js
+++ src/index.js
@@ -5,8 +5,10 @@ import "index.scss";
 import Application from "components/Application";
 import axios from "axios";
 
+console.log("This is Process env -->", process.env);
 if (process.env.REACT_APP_API_BASE_URL) {
   axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL;
+  console.log(axios.defaults.baseURL);
 }
 
 ReactDOM.render(<Application />, document.getElementById("root"));
